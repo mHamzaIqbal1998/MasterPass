1ac0c8e1ade056374f008c3f90b5b37b24101343
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-24 22:32:59 +0500

    fix type error

--- lib/session.ts
+++ lib/session.ts
@@ -2,7 +2,15 @@ import { getServerSession } from "next-auth"
 
 import { authOption } from "@/app/api/auth/[...nextauth]/route"
 
+type USER = {
+  name: string
+  email: string
+  image: string
+}
+type SESSION = {
+  user: USER
+}
 export async function getCurrentUser() {
-  const session = await getServerSession(authOption)
+  const session: SESSION = (await getServerSession(authOption)) as SESSION
   return session?.user
 }
