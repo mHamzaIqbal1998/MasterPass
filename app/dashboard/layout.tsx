d0019626234a598c4f16819f04d67f4641b70e79
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-25 00:38:15 +0500

    dashboard layout added

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+import { notFound } from "next/navigation"
+
+import { getCurrentUser } from "@/lib/session"
+
+interface DashboardLayoutProps {
+  children?: React.ReactNode
+}
+
+export default async function DashboardLayout({
+  children,
+}: DashboardLayoutProps) {
+  const user = await getCurrentUser()
+
+  if (!user) {
+    return notFound()
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col space-y-6">
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
