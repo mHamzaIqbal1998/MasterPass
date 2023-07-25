a237e07ac21c1b43de73eea777511bf77c5261a6
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-25 13:24:33 +0500

    remove runtime

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Features } from "@/components/HomePage/Features"
 import { Hero } from "@/components/HomePage/Hero"
 
 export const dynamic = "force-static"
-export const runtime = "edge"
 
 export default function IndexPage() {
   return (
