c0ab38dc8a0832fee6cc751beed35cb88a70132d
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-25 13:27:08 +0500

    remove dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Features } from "@/components/HomePage/Features"
 import { Hero } from "@/components/HomePage/Hero"
 
-export const dynamic = "force-static"
+export const runtime = "edge"
 
 export default function IndexPage() {
   return (
