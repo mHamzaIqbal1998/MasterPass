f4c178687a428217fd6f28ae35c22c5de3eb687a
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-25 15:06:52 +0500

    fix runtime error

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,7 @@ export const metadata = {
   title: "Dashboard",
 }
 export const dynamic = "force-dynamic"
+export const runtime = "edge"
 
 export default async function Dashboard() {
   return <div>Dashboard</div>
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,3 @@
-import { notFound } from "next/navigation"
-
-import { getCurrentUser } from "@/lib/session"
-
 interface DashboardLayoutProps {
   children?: React.ReactNode
 }
@@ -9,12 +5,6 @@ interface DashboardLayoutProps {
 export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
-  const user = await getCurrentUser()
-
-  if (!user) {
-    return notFound()
-  }
-
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
