bd673728fd751e58f99d21a3bf42bc3b555a2b61
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-25 13:18:38 +0500

    dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Features } from "@/components/HomePage/Features"
 import { Hero } from "@/components/HomePage/Hero"
 
+export const dynamic = "force-dynamic"
+
 export default function IndexPage() {
   return (
     <>
