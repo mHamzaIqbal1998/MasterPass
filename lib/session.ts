525c0b3db6c9ed6b2c0f9b603dc19305d2ea7044
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-25 14:43:53 +0500

    added middleware

--- lib/session.ts
+++ lib/session.ts
@@ -2,15 +2,7 @@ import { getServerSession } from "next-auth"
 
 import { authOptions } from "./auth"
 
-type USER = {
-  name: string
-  email: string
-  image: string
-}
-type SESSION = {
-  user: USER
-}
 export async function getCurrentUser() {
-  const session: SESSION = (await getServerSession(authOptions)) as SESSION
+  const session = await getServerSession(authOptions)
   return session?.user
 }
