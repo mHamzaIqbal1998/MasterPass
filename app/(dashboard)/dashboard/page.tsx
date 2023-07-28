7f8a03ab050d14695e73704713c92ad11b5031c2
HamzaKhan50 <Hamzakh50@gmail.com>
2023-07-29 00:46:21 +0500

    fix legacy install

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,9 +2,6 @@ export const metadata = {
   title: "Dashboard",
 }
 
-export const runtime = "edge"
-export const dynamic = "force-dynamic"
-
 export default async function Dashboard() {
   return <div className="flex justify-center">Coming Soon</div>
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,6 @@
 import { Features } from "@/components/HomePage/Features"
 import { Hero } from "@/components/HomePage/Hero"
 
-export const runtime = "edge"
-export const dynamic = "force-dynamic"
-
 export default function IndexPage() {
   return (
     <>
