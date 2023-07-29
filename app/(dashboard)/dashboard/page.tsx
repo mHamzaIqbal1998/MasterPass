426c769a4261a4c266ecc6526e9dfe87811780bc
M Hamza Iqbal <81909579+mHamzaIqbal1998@users.noreply.github.com>
2023-07-29 21:19:03 +0500

    remove dynamic

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 export const metadata = {
   title: "Dashboard",
 }
-export const dynamic = "force-dynamic"
 export default async function Dashboard() {
   return <div className="flex justify-center">Coming Soon</div>
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,6 @@
 import { Features } from "@/components/HomePage/Features"
 import { Hero } from "@/components/HomePage/Hero"
 
-export const dynamic = "force-dynamic"
 export default function IndexPage() {
   return (
     <>
